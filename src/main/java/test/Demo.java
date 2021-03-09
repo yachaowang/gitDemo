25a8e04f134a538db211660938c5c2d0793c941b
bj <bj@123.com>
2021-03-09 14:02:08 +0800

    add one line

--- src/main/java/test/Demo.java
+++ src/main/java/test/Demo.java
@@ -5,5 +5,6 @@ import org.omg.Messaging.SyncScopeHelper;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git");
     }
 }
